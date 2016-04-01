21e76447d89469ea100703c8a6a7a38ff3aaaf3d
Matt Yang <yang70@gmail.com>
2016-04-01 11:37:21 -0700

    add switch statement for background

--- js/index.js
+++ js/index.js
@@ -8,6 +8,46 @@ $(document).ready(function(){
       temp_c = "",
       weather = "",
       icon = "";
+  
+  function getBackground(weather) {
+    switch(weather) {
+      case "chanceflurries":
+      case "chancesleet":
+      case "sleet":
+      case "chancesnow":
+      case "flurries":
+      case "snow":
+        return "snow";
+        break;
+      case "rain":
+      case "chancerain":
+        return "rain";
+        break;
+      case "tstorms":
+      case "chancestorms":
+        return "tstorms";
+        break;
+      case "partlycloudy":
+      case "partlysunny":
+      case "cloudy":
+      case "hazy":
+      case "partlycloudy":
+      case "mostlycloudy":
+        return "coudy";
+        break;
+      case "fog":
+        return "fog";
+        break;
+      case "clear":
+      case "mostlysunny":
+        return "clear";
+        break;
+      default:
+        return "cloudy";
+        break;
+    }
+  }
+  
   function getWeather(data) {
     lat = data.coords.latitude;
     lon = data.coords.longitude;
@@ -25,8 +65,9 @@ $(document).ready(function(){
         temp_c = data.current_observation.temp_c;
         weather = data.current_observation.weather;
         icon = data.current_observation.icon;
+        console.log(icon);
         
-        $("body").addClass(data.current_observation.icon);
+        $("body").addClass(getBackground(icon));
         $("#wu-link").attr("href", data.current_observation.ob_url);
         $("#temp-f").html(temp_f + " &#8457;");
         $("#temp-c").html(temp_c + " &#8451;");
